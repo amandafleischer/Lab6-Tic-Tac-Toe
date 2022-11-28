82fec1b45e7a61f83fdcfdeee50b97eb8fb1f171
amandafleischer <fleischer.55@osu.edu>
2022-11-28 16:33:38 -0500

    added tags to game objects (testing click input action)

---
+++ Lab6/Assets/Scripts/PlaceObject.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FleischerFouts.Lab6
+{
+    public class PlaceObject : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
