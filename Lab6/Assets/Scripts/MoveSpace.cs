d07992abf0c57a9f9d07112fcea35dcfa3fd8eda
amandafleischer <fleischer.55@osu.edu>
2022-11-28 16:21:18 -0500

    changed input to left mouse button to place

---
+++ Lab6/Assets/Scripts/MoveSpace.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FleischerFouts.Lab6
+{
+    public class MoveSpace : MonoBehaviour
+    {
+        [SerializeField] private GameObject[] firstRow;
+        [SerializeField] private GameObject[] secondRow;
+        [SerializeField] private GameObject[] thirdRow;
+
+        [SerializeField] private GameObject[] firstColumn;
+        [SerializeField] private GameObject[] secondColumn;
+        [SerializeField] private GameObject[] thirdColumn;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
